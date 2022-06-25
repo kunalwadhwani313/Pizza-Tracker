e66cf5392cfba2d729baa19b2ff20dbada12ce26
kunal <kunalwadhwani0090@gmail.com>
2022-06-26 01:17:38 +0530

    created folder structure

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3300
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+
+
+// Global middleware
+app.use((req, res) => {
+   res.render('home')
+})
+// set Template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+app.listen(PORT , () => {
+    console.log(`Listening on port ${PORT}`)
+})
